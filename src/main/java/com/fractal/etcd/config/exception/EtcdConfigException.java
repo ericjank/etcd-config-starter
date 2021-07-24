5bd8f7361fcba82781ba8f4faac0cdc12de70480
老秦 <50172189@qq.com>
2021-07-24 19:10:04 +0800

    Add files via upload

--- src/main/java/com/fractal/etcd/config/exception/EtcdConfigException.java
+++ src/main/java/com/fractal/etcd/config/exception/EtcdConfigException.java
@@ -1,1 +1,16 @@
+package com.fractal.etcd.config.exception;
 
+/**
+ * @author : qinxm
+ * @date : 2021/7/24 9:13 上午
+ */
+public class EtcdConfigException extends RuntimeException {
+
+    public EtcdConfigException(String message) {
+        super(message);
+    }
+
+    public EtcdConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
