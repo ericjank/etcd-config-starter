cfe7194810630747de4118ad2e67ac1c0870edf2
老秦 <50172189@qq.com>
2021-07-24 19:11:06 +0800

    Add files via upload

--- src/main/java/com/fractal/etcd/config/model/ConfigType.java
+++ src/main/java/com/fractal/etcd/config/model/ConfigType.java
@@ -1,1 +1,21 @@
+package com.fractal.etcd.config.model;
 
+/**
+ * @author : qinxm
+ * @date : 2021/7/24 9:14 上午
+ */
+public enum ConfigType {
+    PROPERTIES("properties"),
+
+    YAML("yaml");
+
+    String type;
+
+    ConfigType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ src/main/java/com/fractal/etcd/config/model/EtcdConfigConstants.java
@@ -0,0 +1,11 @@
+package com.fractal.etcd.config.model;
+
+/**
+ * @author : qinxm
+ * @date : 2021/7/24 9:14 上午
+ */
+public class EtcdConfigConstants {
+    public static final String CONFIG_PROPERTIES_PREFIX = "etcd.config";
+
+    public static final String CONFIG_PROPERTIES_ENABLED = "etcd.config.enabled";
+}
